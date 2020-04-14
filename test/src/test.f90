b3fa90a5e65912dc91d3a17e0320e275b4518ce0
Christoph Honal <christoph.honal@web.de>
2020-04-14 08:49:56 +0200

    test dragonegg

--- test/src/test.f90
+++ test/src/test.f90
@@ -1,4 +1,9 @@
+program test
+    implicit none
+end program
+
 function add(a, b) result(res)
+    implicit none
     integer, intent(in)     :: a, b
     integer                 :: res
 
--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd "${0%/*}/../tools"
-docker-compose run --rm -v ../test:/project wasmchain bash -c 'cd /project && make -f makefile.debug build'
-docker run --name testserver -p 8080:3000 -v ../test/bin:/app/public:ro -d tobilg/mini-webserver
+DNAME=`readlink -f ../test`
+docker-compose run --rm -v $DNAME:/project wasmchain bash -c 'cd /project && make -f makefile.debug build'
+docker run --name testserver -p 8080:3000 -v $DNAME/bin:/app/public:ro -d tobilg/mini-webserver
