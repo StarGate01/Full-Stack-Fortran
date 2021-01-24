bd92c37c12785508e8cd57e048ff7a7def888946
Christoph Honal <christoph.honal@web.de>
2021-01-24 02:18:22 +0100

    Update to emscripten 2.0.12

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 cd "${0%/*}/../tools"
 DNAME=`readlink -f ../test`
-docker-compose run --rm -v $DNAME:/project f90wasm bash -c 'cd /project && VERBOSE=1 make build'
+docker-compose run --rm -v $DNAME:/project f90wasm bash -c 'cd /project && VERBOSE=1 EM_CACHE=/project/bin/.emscripten_cache make build'
 docker kill testserver
 docker rm testserver
 docker run --name testserver -p 8080:3000 -v $DNAME/bin:/app/public:ro -d tobilg/mini-webserver
